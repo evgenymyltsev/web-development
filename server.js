5e99e71cccbcf37f8c969a36219d8a77e79f4474
Evgeny Myltsev <myltsev.ea@gmail.com>
2019-03-28 14:35:01 +0300

    add routes

--- server.js
+++ server.js
@@ -1,43 +1,120 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+
+let nextId = 1;
+
+const suggestions = [{
+    id: 1,
+    title: 'Знакомство с серверной разработкой',
+    voters: new Set(),
+}];
+
+const sessions = {};
 
 const server = express();
 
 server.set('view engine', 'pug');
 
 server.use(express.static('public'))
-
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser());
 
+server.use((req, res, next) => {
+    const username = req.cookies.username;
+
+    req.username = username;
+    res.locals.username = username;
+
+    next();
+});
+
+server.use((req, res, next) => {
+    if (req.cookies.sessionId && sessions[req.cookies.sessionId]) {
+        req.session = sessions[req.cookies.sessionId];
+    } else {
+        const sessionId = crypto.randomBytes(8).toString('hex');
+        const session = { id: sessionId };
+
+        sessions[sessionId] = session;
+
+        res.cookie('sessionId', sessionId);
+
+        res.session = session;
+    }
+
+    next();
+});
+
+server.use((req, res, next) => {
+    if (req.session && req.session.message) {
+        res.locals.message = req.session.message;
+        delete req.session.message;
+    }
+
+    next();
+});
+
 server.get('/', (req, res) => {
-    console.log(req.cookies);
-    const { username } = req.cookies;
-    
-    res.render('index', {
-        username
-    });
+    res.render('index');
 });
 
 server.post('/', (req, res) => {
-    res.cookie('username', req.body.username);
+    req.session.username = req.body.username;
+
     res.redirect('/')
 });
 
+server.use((req, res, next) => {
+    if (req.session && !req.session.username) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+});
+
 server.get('/suggestions', (req, res) => {
-    throw new Error('not implemented');
+    res.render('suggestions', {
+        suggestions
+    })
 });
 
 server.post('/suggestions', (req, res) => {
-    throw new Error('not implemented');
+    const title = req.body.title;
+    suggestions.push({
+        id: ++nextId,
+        title,
+        voters: new Set(),
+    });
+
+    req.session.message = 'Предложение принято';
+
+    res.redirect('/suggestions');
 });
 
-server.get('/suggestions/1', (req, res) => {
-    throw new Error('not implemented');
+server.get('/suggestions/:id', (req, res) => {
+    const suggestion = suggestions.find(suggestion =>
+        suggestion.id == req.params.id
+    );
+    res.render('suggestion', {
+        suggestion
+    });
 });
 
-server.post('/suggestions/1', (req, res) => {
-    throw new Error('not implemented');
+server.post('/suggestions/:id', (req, res) => {
+    const username = req.username;
+    const suggestion = suggestions.find(suggestion =>
+        suggestion.id == req.params.id
+    );
+
+    if (suggestion.voters.has(username)) {
+        suggestion.voters.delete(username);
+        req.session.message = 'Голос отменен';
+    } else {
+        suggestion.voters.add(username);
+        req.session.message = 'Голос принят';
+    }
+    res.redirect('back')
 });
 
 server.listen(3000, 'localhost', () => console.log('Server is running'));
